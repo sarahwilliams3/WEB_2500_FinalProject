2fdece228dcf90126c835cfa20a0a800804b43ea
Sarah Foster <sarahtwinwilliams@gmail.com>
2022-12-09 14:13:33 -0700

    netlify

---
+++ src/images/main.js
@@ -0,0 +1,8 @@
+/** @format */
+
+// Required by Webpack - do not touch
+require.context("../fonts/", true, /\.(eot|ttf|woff|woff2)$/i);
+require.context("../images/", true, /\.(png|jpg|jpeg|gif|svg)$/i);
+require.context("../stylesheets/", true, /\.(css|scss)$/i);
+
+//TODO - Your ES6 JavaScript code (if any) goes here
